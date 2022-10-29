ecfd09818ecb4f7ba9939fd954810e2dedce3293
hamzeasadi <hamzeasadi1@gmail.com>
2022-10-29 11:44:46 +0100

    change batch size to 1000 and increase lr from 0.001 to .01

--- src/train.py
+++ src/train.py
@@ -62,10 +62,10 @@ def main():
 
     tnn_model = m.NIRTNN2diff(dp=0.2)
     criterion = utils.NIRLoss()
-    opt = utils.build_opt(Net=tnn_model, opttype='adam', lr=0.001)
+    opt = utils.build_opt(Net=tnn_model, opttype='adam', lr=0.01)
     
     if args.train:
-        train_loader, test_loader = ds.build_dataloader(batch_size=16, noise_level=1e-5, label_scale=0.1)
+        train_loader, test_loader = ds.build_dataloader(batch_size=1000, noise_level=1e-5, label_scale=0.1)
         train(model=tnn_model, train_loader=train_loader, test_loader=test_loader, optimizer=opt, loss_fn=criterion, epochs=args.epoch, wandbf=wbf)
 
     # evaluation
