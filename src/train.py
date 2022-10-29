e50df1ce67ba0f2d8692e5daa28b970d7157ca30
hamzeasadi <hamzeasadi1@gmail.com>
2022-10-29 12:15:11 +0100

    decrease the learning rate 0.0005

--- src/train.py
+++ src/train.py
@@ -62,7 +62,7 @@ def main():
 
     tnn_model = m.NIRTNN2diff(dp=0.2)
     criterion = utils.NIRLoss()
-    opt = utils.build_opt(Net=tnn_model, opttype='adam', lr=0.002)
+    opt = utils.build_opt(Net=tnn_model, opttype='adam', lr=0.0005)
     
     if args.train:
         train_loader, test_loader = ds.build_dataloader(batch_size=4000, noise_level=1e-5, label_scale=0.1)
