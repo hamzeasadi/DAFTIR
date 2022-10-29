49119fe4484ef5b8709e804d9f486b2b1903d6c7
hamzeasadi <hamzeasadi1@gmail.com>
2022-10-29 15:01:41 +0100

    change number of channels

--- src/conf.py
+++ src/conf.py
@@ -26,8 +26,8 @@ paths = dict(
 
 # model template
 model_temp = dict(
-    blk1=dict(inch=2, outch=8, ks=9, stride=5, pool=False, dropout=False),
-    blk2=dict(inch=8, outch=8, ks=4, stride=2, pool=False, dropout=True),
+    blk1=dict(inch=2, outch=16, ks=9, stride=5, pool=False, dropout=False),
+    blk2=dict(inch=16, outch=8, ks=4, stride=2, pool=False, dropout=True),
     blk3=dict(inch=8, outch=8, ks=4, stride=2, pool=False, dropout=True),
     blk4=dict(inch=8, outch=8, ks=3, stride=1, pool=True, dropout=True),
 
