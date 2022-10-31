a82bd634616306eff78c4aafdcfd9e898f897c66
hamzeasadi <hamzeasadi1@gmail.com>
2022-10-31 14:31:24 +0000

    loss with mmd

--- src/conf.py
+++ src/conf.py
@@ -28,10 +28,10 @@ hyper = dict(lr=0.006696, dp=0.2, batch_size=32, opt='adam')
 
 # model template
 model_temp = dict(
-    blk1=dict(inch=3, outch=16, ks=9, stride=5, pool=False, dropout=False),
-    blk2=dict(inch=16, outch=16, ks=4, stride=2, pool=False, dropout=True),
-    blk3=dict(inch=16, outch=8, ks=4, stride=2, pool=False, dropout=True),
-    blk4=dict(inch=8, outch=8, ks=3, stride=1, pool=True, dropout=True),
+    blk1=dict(inch=3, outch=8, ks=9, stride=5, pool=False, dropout=False),
+    blk2=dict(inch=8, outch=16, ks=4, stride=2, pool=False, dropout=True),
+    blk3=dict(inch=16, outch=32, ks=4, stride=2, pool=False, dropout=True),
+    blk4=dict(inch=32, outch=32, ks=3, stride=1, pool=True, dropout=True),
 )
 
 hyper['model'] = model_temp
