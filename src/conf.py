a51e24d413639cc02bee4857b2ebfdf26ca5ad02
hamzeasadi <hamzeasadi1@gmail.com>
2022-11-04 15:01:00 +0000

    change latent representation to 32 channel and no mmd

--- src/conf.py
+++ src/conf.py
@@ -31,7 +31,7 @@ model_temp = dict(
     blk1=dict(inch=2, outch=8, ks=9, stride=5, pool=False, dropout=False),
     blk2=dict(inch=8, outch=16, ks=4, stride=2, pool=False, dropout=True),
     blk3=dict(inch=16, outch=24, ks=4, stride=2, pool=False, dropout=True),
-    blk4=dict(inch=24, outch=24, ks=3, stride=1, pool=True, dropout=True),
+    blk4=dict(inch=32, outch=32, ks=3, stride=1, pool=True, dropout=True),
 )
 
 hyper['model'] = model_temp
